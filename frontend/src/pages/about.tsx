43d340c0977c60e2c9a3a1bec2077ebec0ee6b93
Stanley Mpoyi <mpoyis@oregonstate.edu>
2023-04-22 09:57:34 -0500

    added routes to the frontend folder

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from "react";
+
+export const About: FunctionComponent = () => {
+  return <div>about</div>;
+};
---
+++ frontend/src/pages/newArrival.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from "react";
+
+export const NewArrival: FunctionComponent = () => {
+  return <>New</>;
+};
